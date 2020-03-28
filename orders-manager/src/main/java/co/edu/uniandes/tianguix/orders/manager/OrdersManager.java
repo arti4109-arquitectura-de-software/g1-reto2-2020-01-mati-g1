a067acb058f922328335123f7efc6704abaeaa4c
Juan <Juan.E.Diaz.-ND@disney.com>
2020-03-28 09:27:02 -0500

    Enable actuator for all services

--- orders-manager/src/main/java/co/edu/uniandes/tianguix/orders/manager/OrdersManager.java
+++ orders-manager/src/main/java/co/edu/uniandes/tianguix/orders/manager/OrdersManager.java
@@ -2,11 +2,13 @@ package co.edu.uniandes.tianguix.orders.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author <a href="mailto:d.bellonc@uniandes.edu.co"> Daniel Bellón </a>
  * @since 0.0.1
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrdersManager {
 
